fbc38e1be2e243287fa38305ca27bcdfeee8fa0c
Toni Fisler <mail@tonifisler.ch>
2015-02-24 13:47:17 +0100

    update config capistrano for webfaction server

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,12 @@ ssh_options[:forward_agent] = true
 # APP
 set :application,     "epicgamejam"
 
-set :default_env, { path: "/home/tonifisler/bin:$PATH" }
+# set correct path and drush php for webfaction ser ver
+# (capistrano 2! -> :default_env for capistrano 3)
+set :default_environment, {
+  'PATH' => "/home/tonifisler/bin:$PATH",
+  'DRUSH_PHP' => "/home/tonifisler/bin/php"
+}
 
 # The domain and the path to your app directory
 set :domain,    "staging.epicgamejam.net"
@@ -33,4 +38,6 @@ set :shared_files,    ['drupal/sites/default/settings.php']
 set :download_drush,  true
 
 set  :keep_releases,   3
+
 after "deploy:update", "deploy:cleanup"
+logger.level = Logger::MAX_LEVEL
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,3 +3,6 @@ set :stages, "dev"
 set :application, "stagingegj"
 set :branch,  "dev"
 set :deploy_to, "/home/#{user}/webapps/#{application}"
+
+
+
