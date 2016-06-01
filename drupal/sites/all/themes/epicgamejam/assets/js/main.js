6b2ebc0de0bebda3524ed0f876352c125c5222db
Toni Fisler <mail@tonifisler.ch>
2016-06-01 19:32:46 +0200

    reenable tooltips

--- drupal/sites/all/themes/epicgamejam/assets/js/main.js
+++ drupal/sites/all/themes/epicgamejam/assets/js/main.js
@@ -1,15 +1,17 @@
 'use strict';
 
 (function($) {
+  $(document).ready(function() {
+    $('#countdown').countDown({
+      separator_days: ':',
+      always_show_days: true
+    });
 
-  $('#countdown').countDown({
-    separator_days: ':',
-    always_show_days: true
+    $('.webform-component--epic-locations > input[type=radio]').each(function() {
+      $(this).remove();
+    });
+
+    $('[data-toggle=tooltip]').tooltip();
   });
-
-  $('.webform-component--epic-locations > input[type=radio]').each(function() {
-    $(this).remove();
-  });
-
 })(jQuery);
 
